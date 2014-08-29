f3d6de394d653f2b70bb9837f69c42d873631095
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-29 13:04:38 -0400

    intro progress -- added introExplode

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 // global variable declarations
 // ================================
 
-	var introRollCounter = 1;
+	var introFadeCounter = 1;
+	var introExplodeCounter = 1;
 
 // ================================
 // function declarations
@@ -20,51 +21,77 @@ $(document).ready(function(){
 		$("#blackout").fadeIn(1000);
 		setTimeout(function() {
 			$("#blackout").fadeOut(1000);
-		}, 1000
-
-		);
-		
-
+		}, 1000);
 	}
 
 	function introFade(num) {
-		var introNumber = $('section span:nth-child('+num+')');
+		var introNumber = $('#introFades span:nth-child('+num+')');
 		introNumber.fadeIn(2000);	
-		setTimeout(
-			function() {
-				introNumber.fadeOut(2000);
-			}, 2000
-		);
-	} // end introFade Function
+		setTimeout(function() {
+			introNumber.fadeOut(2000);
+		}, 2000);
+		introFadeCounter++;
+	} 
+
+	function introExplode(num) {
+		var introNumber = $('#introExplode span:nth-child('+num+')');
+		introNumber.fadeIn();
+		setTimeout(function() {
+			introNumber.addClass('blowUp');
+			playExplosion();
+		}, 1000);
+		setTimeout(function() {
+			introNumber.hide();
+		}, 1200);
+		introExplodeCounter++;
+	}
+
+	function introExplodeLast(num) {
+		var introNumber = $('#introExplode span:nth-child('+num+')');
+		introNumber.fadeIn();
+		setTimeout(function() {
+			introNumber.addClass('blowUp');
+			playExplosion();
+		}, 2000);
+		setTimeout(function() {
+			introNumber.hide();
+		}, 2200);
+		introExplodeCounter++;
+	}
 
 	function introRoll() {
-
-		if (introRollCounter < $('section span').length) {
-			introFade(introRollCounter);	
+		if (introFadeCounter < $('#introFades span').length) {
+			introFade(introFadeCounter);	
 			setTimeout(function() {
-				introRollCounter++;
 				introRoll();
 			}, 4000);
-			setTimeout(
-				function() {
-					blink();
-				}, 3000
-			);
-		} else if (introRollCounter === $('section span').length) {
-			introFade(introRollCounter);
-			introRollCounter++;
 			setTimeout(function() {
-				$('section span:last-child').addClass('blowUp');
-				playExplosion().finish();			
+				blink();
+			}, 3000);
+		} else if (introFadeCounter === $('#introFades span').length) {
+			introFade(introFadeCounter);
+			setTimeout(function() {
+				$('#introFades span:last-child').addClass('blowUp');
+				playExplosion();
+				// blink();		
 			}, 2000);
-			setTimeout(
-				function() {
-					blink();
-				}, 2000
-			);
-		} //end if
-
-	
+			setTimeout(function() {
+				introRoll();		
+			}, 4000);
+			setTimeout(function() {
+				blink();		
+			}, 1800);
+		} else if (introExplodeCounter < $('#introExplode span').length) {
+			introExplode(introExplodeCounter);	
+			setTimeout(function() {
+				introRoll();
+			}, 1200);
+		} else if (introExplodeCounter === $('#introExplode span').length) {
+			introExplodeLast(introExplodeCounter);
+			setTimeout(function() {
+				$('#welcome span').fadeIn(5000).delay(1000).fadeOut(5000);
+			}, 2300);
+		}
 	}//end introRoll
 	
 // ================================
